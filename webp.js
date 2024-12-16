e3771b9f92fa84174c67b7f872abdc6922de3376
Takondwa Zulu <tkzulu44@gmail.com>
2024-12-15 17:07:43 -0800

    Initial commit

---
+++ webp.js
@@ -0,0 +1,13 @@
+
+function read (){
+                alert("Welcome to Edgar Lungu National STEM School's official page")
+                
+}
+
+
+
+function changeStyle(){
+                    var text = document.getElementById("button")
+
+
+}
