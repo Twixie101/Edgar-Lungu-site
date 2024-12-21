cc6f068608ada77062baa26a034f0861238072b2
Takondwa Zulu <tkzulu44@gmail.com>
2024-12-21 15:46:12 -0800

    yeah

--- webp.js
+++ webp.js
@@ -1,13 +1,23 @@
 
-function read (){
-                alert("Welcome to Edgar Lungu National STEM School's official page")
+// function read (){
+//                 alert("Welcome to Edgar Lungu National STEM School's official page")
                 
-}
+// }
 
 
+//   function changeStyle(){
+//                     let text = document.getElementById("button") 
+// }
 
-function changeStyle(){
-                    var text = document.getElementById("button") 
+function validateTextbox(){
+    let box = getElementById("username"); 
+    let box2 = getElementById("password")
+    
+    if (box.value == "" || box2.value == "") {
+         alert("Fill in all the red outlined text areas.");
+    return false     
+    
 
-
+    }
+       
 }
